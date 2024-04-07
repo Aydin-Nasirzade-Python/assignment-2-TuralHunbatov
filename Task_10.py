9c94bfc6f38295a18e292f81d36f8080bdae083c
TuralHunbatov <165505681+TuralHunbatov@users.noreply.github.com>
2024-04-07 13:54:29 +0400

    Update Task_10.py

--- Task_10.py
+++ Task_10.py
@@ -1,14 +1,7 @@
 #import libraries here
 
 def main():
-  m=input("Enter name of the month [ex. June]: ")
-  d=int(input("Enter the day [ex. 5]: "))
-  if m=="March" or m=="April" or m=="May" or m=="June":
-      if (m=="March" and d<20) or (m=="June" and d>=21):
-          pass
-      else:
-          print(m,d,"is in Spring")
-  else:
+  
   pass
 
 if __name__ == "__main__":
