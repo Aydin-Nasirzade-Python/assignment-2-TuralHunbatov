86cf83ff0a48d931139cccea8f33d10d2c5e39a7
TuralHunbatov <165505681+TuralHunbatov@users.noreply.github.com>
2024-04-07 14:14:01 +0400

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -1,6 +1,6 @@
 #import libraries here
 def (main):
-    l= input("Enter a letter of the alphabet: ")
+l= input("Enter a letter of the alphabet: ")
     if l=='a' or l=='e' or l=='i' or l=='o' or l=='u':
         print("Entered alphabet is a vowel!")
     elif l=='y':
