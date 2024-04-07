24dacfb2260a13fa16ec7f884b69bcf58586585e
TuralHunbatov <165505681+TuralHunbatov@users.noreply.github.com>
2024-04-07 14:45:49 +0400

    Update Task_4.py

--- Task_4.py
+++ Task_4.py
@@ -1,38 +1,33 @@
 #import libraries here
 
 def main():
-  year = int(input("Enter the year [ex. 2021]: "))
-  animal = ""
-  if year % 12 == 0:
-      animal = "Monkey"
-  elif year % 12 == 1:
-      animal = "Rooster"
-  elif year % 12 == 2:
-      animal = "Dog"
-  elif year % 12 == 3:
-      animal = "Pig"
-  elif year % 12 == 4:
-      animal = "Rat"
-  elif year % 12 == 5:
-      animal = "Ox"
-  elif year % 12 == 6:
-      animal = "Tiger"
-  elif year % 12 == 7:
-      animal = "Hare"
-  elif year % 12 == 8:
-      animal = "Dragon"
-  elif year % 12 == 9:
-      animal = "Snake"
-  elif year % 12 == 10:
-      animal = "Horse"
-  elif year % 12 == 11:
-      animal = "Sheep"
-  if year >= 0:
-      print("%d is the year of the %s" %(year, animal))
-  else:
+  n=int(input("Enter the year [ex. 2021]: "))
+  if n<0:
       print("Invalid year!")
-
-  
+  elif n%12==8:
+      print(n,"is the year of the Dragon")
+  elif n%12==9:
+      print(n,"is the year of the Snake")
+  elif n%12==10:
+      print(n,"is the year of the Horse")
+  elif n%12==11:
+      print(n,"is the year of the Sheep")
+  elif n%12==0:
+      print(n,"is the year of the Monkey")
+  elif n%12==1:
+      print(n,"is the year of the Rooster")
+  elif n%12==2:
+      print(n,"is the year of the Dog")
+  elif n%12==3:
+      print(n,"is the year of the Pig")
+  elif n%12==4:
+      print(n,"is the year of the Rat")
+  elif n%12==5:
+      print(n,"is the year of the Ox")
+  elif n%12==6:
+      print(n,"is the year of the Tiger")
+  elif n%12==7:
+      print(n,"is the year of the Hare")
   pass
 
 if __name__ == "__main__":
