665a3c2ab82462c7928c1c7c9408ad0b50a41876
TuralHunbatov <165505681+TuralHunbatov@users.noreply.github.com>
2024-04-07 14:46:40 +0400

    Update Task_5.py

--- Task_5.py
+++ Task_5.py
@@ -1,34 +1,73 @@
 #import libraries here
 
 def main():
-  month = input("Enter a month [ex. March]: ")
-  day = int(input("Enter the day [ex. 12]: "))
-  if (month == "December" and 22 <= day <= 31) or (month == "January" and 1 <= day <= 19):
-      print('Your zodiac sign is Capricorn')
-  elif (month == "January" and 20 <= day <= 31) or (month == "February" and 1 <= day <= 18):
-      print('Your zodiac sign is Aquarius')
-  elif (month == "February" and 19 <= day <= 29) or (month == "March" and 1 <= day <= 20):
-      print('Your zodiac sign is Pisces')
-  elif (month == "March" and 21 <= day <= 31) or (month == "April" and 1 <= day <= 19):
-      print('Your zodiac sign is Aries')
-  elif (month == "April" and 20 <= day <= 30) or (month == "May" and 1 <= day <= 20):
-      print('Your zodiac sign is Taurus')
-  elif (month == "May" and 21 <= day <= 31) or (month == "June" and 1 <= day <= 20):
-      print('Your zodiac sign is Gemini')
-  elif (month == "June" and 21 <= day <= 30) or (month == "July" and 1 <= day <= 22):
-      print('Your zodiac sign is Cancer')
-  elif (month == "July" and 23 <= day <= 31) or (month == "August" and 1 <= day <= 22):
-      print('Your zodiac sign is Leo')
-  elif (month == "August" and 23 <= day <= 31) or (month == "September" and 1 <= day <= 22):
-      print('Your zodiac sign is Virgo')
-  elif (month == "September" and 23 <= day <= 30) or (month == "October" and 1 <= day <= 22):
-      print('Your zodiac sign is Libra')
-  elif (month == "October" and 23 <= day <= 31) or (month == "November" and 1 <= day <= 21):
-      print('Your zodiac sign is Scorpion')
-  elif (month == "November" and 22 <= day <= 30) or (month == "December" and 1 <= day <= 21):
-      print('Your zodiac sign is Sagittarius')
+  
+  m=input("Enter a month [ex. March]: ")
+  d=int(input("Enter the day [ex. 12]: "))
+    
+  if d>31 or d<0 or m not in ["December","January","February","March","April","May","June","July","August","September","October","November"]or (d>30 and m in ["April","June","September","November"])or(d>29 and m=="February"):
+        print("Either a month or a day is invalid!")
   else:
-      print("Either a month or a day is invalid!")
+      if m=="December" or m=="January":
+          if (m=="December" and d<22)or(m=="January" and d>19):
+              pass
+          else:
+              print("Your zodiac sign is Capricorn")
+      if m=="January" or m=="February":
+          if (m=="January" and d<20)or(m=="February" and d>18):
+              pass
+          else:
+              print("Your zodiac sign is Aquarius")
+      if m=="February" or m=="March":
+          if (m=="February" and d<19)or(m=="March" and d>20):
+              pass
+          else:
+              print("Your zodiac sign is Pisces")
+      if m=="March" or m=="April":
+          if (m=="March" and d<21)or(m=="April" and d>19):
+              pass
+          else:
+              print("Your zodiac sign is Aries")
+      if m=="April" or m=="May":
+          if (m=="April" and d<20)or(m=="May" and d>20):
+              pass
+          else:
+              print("Your zodiac sign is Taurus")
+      if m=="May" or m=="June":
+          if (m=="May" and d<21)or(m=="June" and d>20):
+              pass
+          else:
+              print("Your zodiac sign is Gemini")
+      if m=="June" or m=="July":
+          if (m=="June" and d<21)or(m=="July" and d>22):
+              pass
+          else:
+              print("Your zodiac sign is Cancer")
+      if m=="July" or m=="August":
+          if (m=="July" and d<23)or(m=="August" and d>22):
+              pass
+          else:
+              print("Your zodiac sign is Leo")
+      if m=="August" or m=="September":
+          if (m=="August" and d<23)or(m=="September" and d>22):
+              pass
+          else:
+              print("Your zodiac sign is Virgo")
+      if m=="September" or m=="October":
+          if (m=="September" and d<23)or(m=="October" and d>22):
+              pass
+          else:
+              print("Your zodiac sign is Libra")
+      if m=="October" or m=="November":
+          if (m=="October" and d<23)or(m=="November" and d>21):
+              pass
+          else:
+              print("Your zodiac sign is Scorpion")
+      if m=="November" or m=="December":
+          if (m=="November" and d<22)or(m=="December" and d>21):
+              pass
+          else:
+              print("Your zodiac sign is Sagittarius")
   pass
 
 if __name__ == "__main__":
